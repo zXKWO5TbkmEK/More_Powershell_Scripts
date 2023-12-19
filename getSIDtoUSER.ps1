40a6550f375a3cc4a6d13e557395de024271771f
zXKWO5TbkmEK <140012250+zXKWO5TbkmEK@users.noreply.github.com>
2023-12-19 15:41:57 -0600

    Update getSIDtoUSER.ps1

--- getSIDtoUSER.ps1
+++ getSIDtoUSER.ps1
@@ -1,5 +1,5 @@
 # Give SID as input to .NET Framework Class
-$SID = New-Object System.Security.Principal.SecurityIdentifier("S-1-5-21-4256735972-1205184747-4227965648-30616")
+$SID = New-Object System.Security.Principal.SecurityIdentifier("SET SID HERE")
 
 # Use Translate to find user from sid
 $objUser = $SID.Translate([System.Security.Principal.NTAccount])
